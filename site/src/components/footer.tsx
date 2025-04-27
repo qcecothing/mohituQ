af84ff4f91b695bd3125208d6587aea599d04aa2
Javier R <javier.rios-urbano@epitech.eu>
2025-04-27 10:12:19 +0400

    fix: use `neutral` color in footer

--- site/src/components/footer.tsx
+++ site/src/components/footer.tsx
@@ -10,7 +10,7 @@ import {
 
 export function FooterComponent() {
   return (
-    <footer className="bg-black text-white py-12 mt-12">
+    <footer className="bg-neutral-800 text-white py-12 mt-12">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div>
